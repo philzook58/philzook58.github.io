e6b98b3df13cc5105eb27fe8999503843aeb3d3d
Philip Zucker <philzook58@gmail.com>
2025-07-25 17:58:20 -0400

    doot

---
+++ _drafts/fstar.fst
@@ -0,0 +1,11 @@
+(*
+https://fstar-lang.org/tutorial/
+
+https://fstar-lang.org/tutorial/book/pulse/pulse.html Pulse: Proof-oriented Programming in Concurrent Separation Logic
+*)
+
+// line comment
+
+let mytt : Unit = tt
+
+// #lang-pulse
---
+++ _drafts/iris.v
@@ -0,0 +1,25 @@
+(*
+
+Iris
+RefinedC
+VST
+Separation something in 
+
+https://gitlab.mpi-sws.org/iris/iris
+
+https://people.mpi-sws.org/~dreyer/papers/iris-ground-up/paper.pdf iris from the ground up
+
+https://iris-project.org/pdfs/2017-esop-iris3-final.pdf The essence of higher order separation logic
+
+Later modality for step indexed relations https://arxiv.org/abs/1103.0510
+Step indexing is interesing for interpreting non terminating types? What about Bool + Int with some unbounded thing and no binders. step _indexed_ as in indices vs parameters?
+
+Lob induction?
+
+https://iris-project.org/pdfs/2023-phd-gratzer.pdf Syntax and semantics of modal type theory
+
+PCM - partial commutative monoid
+
+https://arxiv.org/abs/2105.12077 A beginner guide to Iris, Coq and separation logic
+
+*)
